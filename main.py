09e17d4a78baaf49099c573272785d622257ce02
nicolemolineros <48654860+nicolemolineros@users.noreply.github.com>
2024-09-05 20:13:14 -0500

    [UPDATE] main.py

--- main.py
+++ main.py
@@ -39,41 +39,53 @@ def play_sound(filename, lado=0):
         source.stop()
         
 
-def sonido_muerte_bruja():
-    play_sound("sonidos/muerte_bruja.wav")
+def witch_sound():
+    play_sound("sound_effects/witchdeath.wav")
 
-def sonido_muerte_goblin():
-    play_sound("sonidos/muerte_goblin.wav")
+def goblin_sound():
+    play_sound("sound_effects/goblinattack.wav") 
 
-def sonido_muerte_dragón():
-    play_sound("sonidos/muerte_dragon.wav")
+def dragon_sound():
+    play_sound("sound_effects/dragon_sound.wav") 
 
-def sonido_muerte_monstruo():
-    play_sound("sonidos/muerte_monstruo.wav")
+def monster_sound():
+    play_sound("sound_effects/monster.wav") 
 
-def sonido_victoria():
-    play_sound("sonidos/victoria.wav")
+def victory_sound():
+    play_sound("sound_effects/victory.wav") 
 
-def sonido_derrota():
-    play_sound("sonidos/derrota.wav")
+def defeat_sound():
+    play_sound("sound_effects/defeat.wav") 
 
-def sonido_curar():
-    play_sound("sonidos/curar.wav")
+def restore_sound():
+    play_sound("sound_effects/recover.wav") 
 
-def sonido_atacar():
-    play_sound("sonidos/atacar.wav")
+def attack_sound(): 
+    play_sound("sound_effects/attack.wav") 
 
-def sonido_ataque_fallido():
-    play_sound("sonidos/ataque_fallido.wav")
+def failed_attack_sound(): 
+    play_sound("sound_effects/failattack.wav")
 
-def sonido_ataque_dragon():
-    play_sound("sonidos/ataque_dragon.wav")
+def dragon_attack_sound(): 
+    play_sound("sound_effects/dragonattack.wav")
 
-def sonido_ataque_bruja():
-    play_sound("sonidos/ataque_bruja.wav")
+def witch_attack_sound(): 
+    play_sound("sound_effects/witchattack.wav")
 
-def sonido_ataque_goblin():
-    play_sound("sonidos/ataque_goblin.wav")
+def goblin_attack_sound():
+    play_sound("sound_effects/goblindeath.wav") 
+
+def potion_sound():
+    play_sound("sound_effects/potion.wav") 
+
+def help_sound():
+    play_sound("sound_effects/help.wav", 1,0,0) 
+
+def bells_sound():
+    play_sound("sound_effects/bells.wav") 
+
+def laugh_sound():
+    play_sound("sound_effects/laugh.wav", 0,0,-1)
 
 def print_status(player, enemy):
     print(f"\nTu vida: {player.health}")
@@ -95,29 +107,29 @@ def combat(player : Player, enemy):
                 damage = player.attack
                 enemy.health -= damage
                 print(f"¡Atacas al {enemy.name} y le haces {damage} de daño!")
-                sonido_atacar()
+                attack_sound()
             else:
                 print("Fallas el ataque.")
-                sonido_ataque_fallido()
+                failed_attack_sound()
         elif choice == '2':
             heal = random.randint(0, player.max_health//6)
             player.health = min(player.health + heal, player.max_health)
             print(f"Te curas {heal} puntos de vida.")
-            sonido_curar()
+            restore_sound()
         else:
             print("Opción no válida. Pierdes tu turno.")
         
         if enemy.health <= 0:
             print(f"¡Has derrotado al {enemy.name}!")
             if enemy.name == "Monstruo Terrible":
-                sonido_muerte_monstruo()
+                monster_sound()
             elif enemy.name == "Goblin":
-                sonido_muerte_goblin()
+                goblin_sound()
             elif enemy.name == "Bruja":
-                sonido_muerte_bruja()
+                witch_sound()
             elif enemy.name == "Dragón Feroz":
-                sonido_muerte_dragón()
-            sonido_victoria()
+                dragon_sound()
+            victory_sound()
             return "victory"
         
         # Turno del enemigo
@@ -125,15 +137,15 @@ def combat(player : Player, enemy):
         player.health -= enemy_damage
         print(f"El {enemy.name} te ataca y te hace {enemy_damage} de daño.")
         if enemy.name == "Dragón Feroz":
-            sonido_ataque_dragon()
+            dragon_attack_sound()
         elif enemy.name == "Bruja":
-            sonido_ataque_bruja()
+            witch_attack_sound()
         elif enemy.name == "Goblin":
-            sonido_ataque_goblin()
+            goblin_attack_sound()
     
     if player.health <= 0:
         print("Has sido derrotado...")
-        sonido_derrota()
+        defeat_sound()
         return "defeat"
 
 def inicio(player : Player):
@@ -147,6 +159,7 @@ def inicio(player : Player):
     if resultado == "defeat":
         print("El monstruo te ha derrotado, pero no todo está perdido...")
         print("Despiertas en una celda, herido pero vivo.")
+        restore_sound()
         player.health = player.max_health // 2  # Recuperas la mitad de tu salud
         return True
     else:
@@ -165,27 +178,33 @@ def nudo_1(player : Player):
         print("La llave abre un cofre cercano. ¡Dentro hay una espada mágica!")
         player.attack = random.randint(3, 5)
         print(f"Tu ataque ha aumentado a {player.attack}.")
+        restore_sound()
         return True
     else:
         print("Has sido derrotado por el goblin. Tu aventura termina aquí.")
+        defeat_sound()
         return False
 
 def nudo_2(player : Player):
     print("\n--- ACTO II: EL NUDO PARTE 2 ---")
     print("Despues de matar al goblin, encuentras una habitacion. Entras y hay una bruja")
-    
+    laugh_sound()
+
     enemigo_debil = Enemy("Bruja", 10, 1)
     resultado = combat(player, enemigo_debil)
     
     if resultado == "victory":
         print("Has derrotado a la bruja. En su estante encuentras una pocion.")
+        witch_sound()
         print("Te tomas la pocion y recuperas todos tus puntos de vida.")
+        potion_sound()
         player.health = player.max_health
         print(f"Tu vida ha aumentado a {player.health}.")
-        sonido_curar()
+        restore_sound()
         return True
     else:
         print("Has sido derrotado por la bruja. Tu aventura termina aquí.")
+        defeat_sound()
         return False
 
 def desenlace_1(player : Player):
@@ -198,15 +217,18 @@ def desenlace_1(player : Player):
     if resultado == "victory":
         print("¡Has derrotado al dragón y rescatado a la princesa!")
         print("Felicidades, has completado tu misión.")
+        victory_sound()
         return True
     else:
         print("El dragón era demasiado poderoso. Tu aventura termina aquí.")
+        defeat_sound()
         return False
     
 def desenlace_2():
     print("\n--- ACTO III: EL DESENLACE PARTE 2---")
     print("Escuchas a la princesa gritar a tu derecha. Corres hacia ella")
-    
+    help_sound()
+
     print("La princesa te pide que la liberes de sus cadenas")
     print("Con tu espada, cortas las cadenas de la princesa")
     print("La princesa te da las gracias y te da un beso en la mejilla")
@@ -215,21 +237,21 @@ def desenlace_2():
 
     print("La princesa te lleva a su reino y te da una recompensa")
     print("La recompensa es casarte con ella y ser el rey de su reino")
-
-    sonido_victoria()
+    bells_sound()
+    
     return True
 
 def main_game_loop():
     nombre = input("Ingresa el nombre de tu héroe: ")
     player = Player(nombre, health=15, attack=2)
     print(f"Bienvenido, {player.name}. Tu misión es rescatar a la princesa de que se encuentra encerrada hace quince años en una mansión abandonada llena de monstruos y criaturas.")
-    sonido_muerte_bruja()
     if inicio(player):
         if nudo_1(player):
             if nudo_2(player):
                 if desenlace_1(player):
                     desenlace_2()
                     print("¡Has completado el juego! Eres un verdadero héroe.")
+                    victory_sound()
 
     print(f"Gracias por jugar {player.name}.")
     oalQuit()
